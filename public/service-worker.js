87a928546a3b5271cd2cc61ff5874361fbbdbcca
Adam Olson <adamlsn@gmail.com>
2021-07-20 18:17:30 -0500

    working version of service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,62 @@
+const { response } = require("express");
+const { version } = require("mongoose");
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js",
+    "./icons/icon-72x72.png", 
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"    
+];
+
+const APP_PREFIX = "Budget-";
+const VERSION = "version_1";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log(`Installing Chache: ${CACHE_NAME}`);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    caches.waitUntil(
+        caches.keys().then(function(keys){
+            let cacheKeepList = keys.filter(function(key){
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keys.map(function(key, i){
+                    if(cacheKeepList.indexOf(key) === -1){
+                        console.log(`Deleting Cache: ${keys[i]}`);
+                        return caches.delete(keys[i]);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match("./index.html");
+            })
+    )
+});
